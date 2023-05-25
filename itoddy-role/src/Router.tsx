7d8b7d5e6275ffdea94197ab20a0e5a839ec4791
ViniciusdePSouza <dev.souzavinicius@gmail.com>
2023-05-25 10:27:36 -0300

    Ci: first branch commit

--- itoddy-role/src/Router.tsx
+++ itoddy-role/src/Router.tsx
@@ -1,10 +1,12 @@
 import {Route, Routes} from 'react-router-dom'
 import { SignIn } from './Pages/SignIn'
+import { SignUp } from './Pages/SignIn/SignUp'
 
 export function Router() {
     return(
         <Routes>
             <Route path='/iToddy_Role' element={<SignIn/>}/>
+            <Route path='/iToddy_Role/signup' element={<SignUp/>}/>
         </Routes>
     )
 }
