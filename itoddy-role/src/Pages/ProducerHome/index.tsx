3e4b4897483ca5a2d4ac1dc12026f881c2fe3930
ViniciusdePSouza <dev.souzavinicius@gmail.com>
2023-05-26 15:41:34 -0300

    Styles: HomeProducer page tab component in progress

--- itoddy-role/src/Pages/ProducerHome/index.tsx
+++ itoddy-role/src/Pages/ProducerHome/index.tsx
@@ -1,5 +1,49 @@
+import {
+  Container,
+  HomeProducerHeader,
+  Logo,
+  NavButtonWrapper,
+  ProducerNav,
+  TabList,
+  TabRoot,
+  TabTrigger,
+} from "./styles";
+
+import logo from "../../assets/purpleLogo.svg";
+import profileIcon from "../../assets/profile.svg";
+import exitIcon from "../../assets/exit.svg";
+import searchIcon from "../../assets/search.svg";
+
+import { SvgButton } from "../../components/SvgButton";
+import Input from "../../components/Input";
+
 export function ProducerHome() {
-    return (
-        <h1>Home Producer Works ! </h1>
-    )
+  return (
+    <>
+      <HomeProducerHeader>
+        <Logo src={logo} alt="logo" />
+      </HomeProducerHeader>
+      <Container>
+        <ProducerNav>
+          <h1>Meus Eventos</h1>
+          <NavButtonWrapper>
+            <SvgButton svg={profileIcon} variant="PRIMARY" />
+            <SvgButton svg={exitIcon} variant="PRIMARY" />
+          </NavButtonWrapper>
+        </ProducerNav>
+        <Input icon={searchIcon} placeholder="pesquisar meus eventos" />
+
+        <TabRoot defaultValue="tab1">
+          <TabList aria-label="Gerencie seus eventos">
+            <TabTrigger value="tab1">
+                A seguir
+            </TabTrigger>
+            <TabTrigger value="tab2">
+                Eventos passados
+            </TabTrigger>
+          </TabList>
+        </TabRoot>
+      </Container>
+    </>
+  );
 }
---
+++ itoddy-role/src/Pages/ProducerHome/styles.ts
@@ -0,0 +1,80 @@
+import { styled } from "styled-components";
+import * as Tabs from "@radix-ui/react-tabs";
+
+export const Container = styled.div`
+  width: 100%;
+
+  padding: 1.5rem;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+export const Logo = styled.img`
+  margin: 1rem;
+`;
+
+export const ProducerNav = styled.nav`
+  width: 100%;
+
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  margin-bottom: 1.6rem;
+
+  > h1 {
+    font-weight: 600;
+    font-size: 2rem;
+    line-height: 2.4rem;
+  }
+`;
+
+export const NavButtonWrapper = styled.div`
+  display: flex;
+  align-items: center;
+
+  gap: 1.6rem;
+`;
+
+export const TabRoot = styled(Tabs.Root)`
+  margin-top: 1.2rem;
+`;
+export const TabList = styled(Tabs.List)`
+  width: 100%;
+
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+
+  gap: 2.4rem;
+`;
+export const TabTrigger = styled(Tabs.Trigger)`
+  all: unset;
+
+  font-family: "Nunito", sans-serif;
+  font-size: 2rem;
+  font-weight: 600;
+  line-height: 2.4rem;
+
+  color: ${({ theme }) => theme.COLORS.SECONDARY};
+
+  &[data-state="active"] {
+    border-bottom: 1px solid ${({ theme }) => theme.COLORS.SECONDARY};
+    color: ${({ theme }) => theme.COLORS.PURPLE_400};
+  }
+`;
+
+export const HomeProducerHeader = styled.header`
+  width: 100%;
+
+  padding: 2.4rem;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  -webkit-box-shadow: 0px 4px 5px 0px rgba(181, 177, 181, 1);
+  -moz-box-shadow: 0px 4px 5px 0px rgba(181, 177, 181, 1);
+  box-shadow: 0px 4px 5px 0px rgba(217, 208, 217, 1);
+`;
